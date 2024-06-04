921b08c7b60fd0619a61b319696ee72ca9e8e33b
Brian Corbita <btcorbita@gmail.com>
2024-06-04 02:13:37 -0700

    Command Loading
    # Implementing Ability To Load Commands For Bot Use
    - Corrected and added comment documentation
    - Added guild intent to client to ensure data is stored within discord server
    - Added requirements to identify and access command files
    - Added function for bot to load commands into function for use when triggered with debug message

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-//Establish libraries
-const {Client,Events,GatewayIntentBits} = require("discord.js");
-const {token} = require("./config.json");
-//Declare client
-const client = new Client({intents: []});
+//Establish requirement
+const fs = require('node:fs'); //identifies command files
+const path = require('node:path'); //assistance for accessing files and directories
+const {Client,Collection,Events,GatewayIntentBits} = require("discord.js"); //access discord library
+const {token} = require("./config.json"); //bot identifier
 
+//Declare client
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+//Stores and Retrieves commands for execution
+client.commands = new Collection();
+//Establish command location
+const foldersPath = path.join(__dirname, "commands"); //path to commands
+const commandFolders = fs.readdirSync(foldersPath); //path to command subfolders
+//Goes through subfolders
+for(const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder); //open subfolder
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js")); //access command files grouped in subfolder
+    //Goes through command group in subfolder
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file); //open command
+        const command = require(filePath); //access command
+        //Checks command for required identification and functionality properties
+        if('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command); //Execute
+        } else {
+            console.log(`[WARNING] Command at ${filePath} is missing require "data" or "execute" property.`)
+        }
+    }
+}
+
+//Console intro message
 client.once(Events.ClientReady, readyClient => {
     console.log(`Activating...${readyClient.user.tag} is online.`)
 })
