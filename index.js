8e89e257bad18b51d1353c74951c00a14ef28623
Brian Corbita <btcorbita@gmail.com>
2024-06-04 02:50:31 -0700

    Command Handler
    # Implement Processor To Read And Execute Command Files
    - Corrected and added comment documentation
    - Added function for bot to detect slash commands in server
    - Added function for bot to execute commands with error checking

--- index.js
+++ index.js
@@ -21,12 +21,34 @@ for(const folder of commandFolders) {
         const command = require(filePath); //access command
         //Checks command for required identification and functionality properties
         if('data' in command && 'execute' in command){
-            client.commands.set(command.data.name, command); //Execute
+            client.commands.set(command.data.name, command); //load command
         } else {
             console.log(`[WARNING] Command at ${filePath} is missing require "data" or "execute" property.`)
         }
     }
 }
+//Listens for interactions
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return; //Checks for non command interactions
+    //Obtain command interaction
+    const command = interaction.client.commands.get(interaction.commandName);
+    //Checks if command exists
+    if(!command) {
+        console.error(`${interaction.commandName} not found.`);
+        return;
+    }
+    //Execute command
+    try {
+        await command.execute(interaction);
+    } catch (error) { //Logs when error occurs with command functionality
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: "Error with executing command", ephemeral: true }); //When error occurs post-output or timeout
+        } else {
+            await interaction.reply({ content: "Error with executing command", ephemeral: true }); //When error occurs pre-output
+        }
+    }
+})
 
 //Console intro message
 client.once(Events.ClientReady, readyClient => {
